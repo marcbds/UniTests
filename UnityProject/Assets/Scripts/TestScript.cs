7e3d19f5e72c66dad3c05915746ca6d7b73824ed
marc.balsadiaz@gmail.com <marc.balsadiaz@gmail.com>
2022-03-28 17:37:02 +0200

    Debug al start

--- UnityProject/Assets/Scripts/TestScript.cs
+++ UnityProject/Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Projecte2");
     }
 
     // Update is called once per frame

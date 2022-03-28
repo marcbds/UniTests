67b024c88324d7ebdc68eda7de8f53a3e35bd3f5
marc.balsadiaz@gmail.com <marc.balsadiaz@gmail.com>
2022-03-28 17:38:39 +0200

    canvis start i update

--- UnityProject/Assets/Scripts/TestScript.cs
+++ UnityProject/Assets/Scripts/TestScript.cs
@@ -7,12 +7,12 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Projecte1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 5;
     }
 }
